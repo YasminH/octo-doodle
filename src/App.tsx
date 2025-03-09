db89ddb1fcb77b38fe19673d6c35b4d99d27140a
Y.H. <yhabayeb@gmail.com>
2025-03-09 18:45:45 +0100

    Fetch the items from json and display them as checkboxes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const url = "./items.json";
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setItems(data.data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
-    <h1>
-      hello world
-    </h1>
-  )
+    <ul>
+      {items?.length &&
+        items.map((item, index) => (
+          <li key={index}>
+            <input type="checkbox" value={item} name={item} id={item} />
+            <label htmlFor={item} dangerouslySetInnerHTML={{ __html: item }} />
+          </li>
+        ))}
+    </ul>
+  );
 }
 
-export default App
+export default App;
