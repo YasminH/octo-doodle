e18474015164698d1bd2c64f8ea220b23977863f
Y.H. <yhabayeb@gmail.com>
2025-03-12 20:26:20 +0100

    Add app styling and some other general styling improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
 import MultiSelectFilter from "./MultiSelectFilter/MultiSelectFilter";
+import styles from "./App.module.scss";
 
 export default function App() {
-  return <MultiSelectFilter />;
+  return (
+    <>
+      <header>
+        <h1 className={styles.title}>Multi select filter</h1>
+      </header>
+      <main>
+        <MultiSelectFilter />
+        <div className={styles.placeholder}>Placeholder for listing</div>
+      </main>
+    </>
+  );
 }
