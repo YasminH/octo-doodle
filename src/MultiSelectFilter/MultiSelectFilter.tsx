75ebc3a029c42745f47c90afe481904bbb68c080
Y.H. <yhabayeb@gmail.com>
2025-03-12 19:41:47 +0100

    Move pill html and styling to its own component

--- src/MultiSelectFilter/MultiSelectFilter.tsx
+++ src/MultiSelectFilter/MultiSelectFilter.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Checkbox from "../Checkbox/Checkbox";
 import SearchBox from "../SearchBox/SearchBox";
 import styles from "./MultiSelectFilter.module.scss";
+import Pill from "../Pill/Pill";
 
 export default function MultiSelectFilter() {
   const [items, setItems] = useState<string[]>([]);
@@ -57,34 +58,30 @@ export default function MultiSelectFilter() {
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>Productgroep</h2>
-      <div>
-        <SearchBox onChange={handleSearchBoxChange} />
-        {activeFilters?.length > 0 && (
-          <div className={styles.pillGroup}>
-            Gekozen filters:
-            {activeFilters.map((item, index) => (
-              <span
-                key={index}
-                dangerouslySetInnerHTML={{ __html: item }}
-                className={styles.pill}
+      <SearchBox onChange={handleSearchBoxChange} />
+      {activeFilters?.length > 0 && (
+        <div className={styles.activeFilters}>
+          Gekozen filters:
+          {activeFilters.map((item, index) => (
+            <Pill key={index}>
+              <span dangerouslySetInnerHTML={{ __html: item }} />
+            </Pill>
+          ))}
+        </div>
+      )}
+      {filteredItems?.length > 0 && (
+        <ul className={styles.list}>
+          {filteredItems.map((item) => (
+            <li key={item} className={styles.listItem}>
+              <Checkbox
+                value={item}
+                onChange={handleCheckboxChange}
+                checked={activeFilters.includes(item)}
               />
-            ))}
-          </div>
-        )}
-        {filteredItems?.length > 0 && (
-          <ul className={styles.list}>
-            {filteredItems.map((item) => (
-              <li key={item} className={styles.listItem}>
-                <Checkbox
-                  value={item}
-                  onChange={handleCheckboxChange}
-                  checked={activeFilters.includes(item)}
-                />
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
---
+++ src/Pill/Pill.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+import styles from "./Pill.module.scss";
+
+type PillProps = {
+  children: ReactNode;
+};
+
+export default function Pill({ children }: PillProps) {
+  return <span className={styles.pill}>{children}</span>;
+}
