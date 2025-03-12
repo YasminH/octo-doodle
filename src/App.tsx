f324c0cbfc9b73e8a8c07c7f54a8c08e1ead426e
Y.H. <yhabayeb@gmail.com>
2025-03-12 20:43:30 +0100

    Fix grid issue. Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,17 @@ export default function App() {
   return (
     <>
       <header>
-        <h1 className={styles.title}>Multi select filter</h1>
+        <h1 className={styles.title}>Multi select filter </h1>
       </header>
       <main>
         <MultiSelectFilter />
         <div className={styles.placeholder}>Placeholder for listing</div>
       </main>
+      <footer>
+        <a href="https://github.com/YasminH/octo-doodle" target="_blank">
+          🍰 Y.H.
+        </a>
+      </footer>
     </>
   );
 }
